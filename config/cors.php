508022e965df0fc7f8755a22f248b41718f34a26
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-03-16 17:21:55 +0100

    allowed cors

---
+++ config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'login', 'logout'], // Define routes that should allow CORS
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],  // You can specify a domain instead of '*'
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
