7a281b41453194e30c4628ab289f955e4c976703
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-03-19 12:29:44 +0100

    added coalscing to validateddata

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{assets('images/logo/smile-logistics-dark.png')}}" class="logo" alt="smile Logo">
+<img src="{{asset('images/logo/smile-logistics-dark.png')}}" class="logo" alt="smile Logo">
 @else
 {{ $slot }}
 @endif
