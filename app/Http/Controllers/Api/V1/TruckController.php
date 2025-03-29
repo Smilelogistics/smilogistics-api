0a2cbf92d456164bf0031e6f4e23c12fe1b5ef77
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-03-29 13:06:14 +0100

    debugging update method

--- app/Http/Controllers/Api/V1/TruckController.php
+++ app/Http/Controllers/Api/V1/TruckController.php
@@ -186,7 +186,7 @@ class TruckController extends Controller
         $authUser = auth()->user();
         $branchId = $authUser->branch ? $authUser->branch->id : null;
 
-        return response()->json(['request' => $request->all()], 200);
+        return response()->json(['request_data' => $request->all()], 200);
 
         try {
             $validateTruck = Validator::make($request->all(), [
