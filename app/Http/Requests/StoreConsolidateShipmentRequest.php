40c0ce25d93815c0776370acc5a31429b1a4da83
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-04-27 10:46:36 +0100

    fix file path

--- app/Http/Requests/StoreConsolidateShipmentRequest.php
+++ app/Http/Requests/StoreConsolidateShipmentRequest.php
@@ -59,7 +59,8 @@ class StoreConsolidateShipmentRequest extends FormRequest
             'file_path.*' => 'nullable|file|mimes:pdf,jpg,jpeg,png',
             'file_path.*' => 'nullable|file|mimes:pdf,jpg,png,jpeg,doc,docx|max:2048',
             'file_path' => 'sometimes|array',
-            'proof_of_delivery_path.*' => 'nullable|file|mimes:pdf,jpg,jpeg,png',
+            'proof_of_delivery_path' => 'nullable|file|mimes:pdf,jpg,jpeg,png',
+            'invoice_path' => 'nullable|file|mimes:pdf,jpg,jpeg,png',
             'internal_notes' => 'nullable|string|max:500',
 
         ];
