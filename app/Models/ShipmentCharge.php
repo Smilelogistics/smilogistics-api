4e404d9ab60e4e29d24d6b1608a0c6f21beed6a0
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-05-15 11:55:45 +0100

    expense totals

--- app/Models/ShipmentCharge.php
+++ app/Models/ShipmentCharge.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ShipmentCharge extends Model
 {
     protected $table = 'shipment_charges';
-    protected $fillable =  ['branch_id', 'charge_type', 'comment', 'units', 'rate', 'amount', 'discount', 'internal_notes', 'billed', 'invoice_number', 'invoice_date', 'total', 'net_total', 'shipment_id'];
+   // protected $fillable =  ['branch_id', 'charge_type', 'comment', 'units', 'rate', 'amount', 'discount', 'internal_notes', 'billed', 'invoice_number', 'invoice_date', 'total', 'net_total', 'shipment_id'];
+
+   protected $guarded = [];
 
     public function shipment()
     {
