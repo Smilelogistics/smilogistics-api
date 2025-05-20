2df3589e63dcd6730449602b9fd7bba7387cc3d3
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-05-20 22:53:00 +0100

    railway

--- config/cors.php
+++ config/cors.php
@@ -14,21 +14,14 @@
 // ];
 
 return [
-
-    'paths' => ['api/*', 'login', 'logout'],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:3000'], // ✅ Use exact origin
-
-    'allowed_origins_patterns' => [],
-
+    'allowed_origins' => [
+        'https://smileslogistics-frontend.vercel.app',
+        'http://localhost:3000' // for local development
+    ],
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
-
+    'supports_credentials' => false,
 ];
