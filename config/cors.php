d6029c015e8722aaf971e8946b7b3fbfbc02b3c9
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-05-20 23:17:32 +0100

    railway

--- config/cors.php
+++ config/cors.php
@@ -1,27 +1,14 @@
 <?php
 
-// return [
-//     'paths' => ['api/*', 'login', 'logout'], // Define routes that should allow CORS
-//     'allowed_methods' => ['*'],
-//     'allowed_origins' => ['*'],  // You can specify a domain instead of '*'
-//     'allowed_origins_patterns' => [],
-//     'allowed_headers' => ['*'],
-//     'exposed_headers' => [],
-//     'max_age' => 0,
-//     'supports_credentials' => true,
-//     //'allowed_headers' => ['Authorization', 'Content-Type', 'X-Requested-With'],
-
-// ];
-
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', 'login', 'logout'], // Define routes that should allow CORS
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://smileslogistics-frontend.vercel.app',
-        'http://localhost:3000' // for local development
-    ],
+    'allowed_origins' => ['*'],  // You can specify a domain instead of '*'
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
+    //'allowed_headers' => ['Authorization', 'Content-Type', 'X-Requested-With'],
+
 ];
