86f9b5ad6e2ea22e0102d844182f98c350d192b9
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-06-02 22:46:17 +0100

    superadmin settings

--- database/migrations/2025_06_02_205158_create_super_admin_settings_table.php
+++ database/migrations/2025_06_02_205158_create_super_admin_settings_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('super_admin_settings', function (Blueprint $table) {
+        Schema::table('super_admin_settings', function (Blueprint $table) {
             $table->id();
             $table->string('invoice_prefix', 10)->nullable();
             $table->string('invoice_logo')->nullable();
