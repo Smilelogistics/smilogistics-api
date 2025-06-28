6912a6df191c4501148ffb6c03eefeba02b8aa7a
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-06-28 13:38:48 +0100

    plans

--- database/seeders/PlansSeeder.php
+++ database/seeders/PlansSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Plan;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PlansSeeder extends Seeder
 {
