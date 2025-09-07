b4a1cc8258a1be24a39653788c71e334fb58a096
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-09-07 12:19:08 +0100

    truck edit fixed

--- app/Http/Controllers/Api/V1/TruckController.php
+++ app/Http/Controllers/Api/V1/TruckController.php
@@ -134,7 +134,7 @@ class TruckController extends Controller
             $truck = Truck::create([
                 'branch_id' => $branchId,
                 'user_id' => $authUser->id,
-                'office' => $validateTruck['truck_office'],
+                 'office'    => $validatedTruck['office'] ?? null,
                 ...$validatedTruck
             ]);
 
