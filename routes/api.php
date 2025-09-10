dcee44d1c9e2f8da47e49a45a01301be7fc09e55
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-09-10 18:38:29 +0100

    flutterwave payment integration

--- routes/api.php
+++ routes/api.php
@@ -183,7 +183,7 @@ Route::get('/config/maps', [UnivController::class, 'getMapsData']);
     // Handle reset submission
     Route::post('/reset-password', [AuthController::class, 'resetPassword'])->middleware('guest')->name('password.update');
     Route::get('/verify-paystack/{reference}', [TransactionsController::class, 'verifyPaymentAPI']);
-    Route::get('/verify-payment/flutterwave', [TransactionsController::class, 'verifyFlutterwavePayment']);
+    Route::get('/verify-payment/flutterwave/{transaction_id}/{tx_ref}/{status}', [TransactionsController::class, 'verifyFlutterwavePayment']);
     // Route::get('/verify/flutterwave/{reference?}', [TransactionsController::class, 'callbackFlutterwave']);
 
     //Route::get('/verify-paystack', [TransactionsController::class, 'verifyPaysatckPayment']);
