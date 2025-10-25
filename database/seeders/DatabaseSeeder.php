aeb02f07c341f43afa741f7bf61a481b8ab65cc8
Barnabas KolozJnr <67397553+kolozjnr@users.noreply.github.com>
2025-10-25 17:25:14 +0100

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\SuperAdmin;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +16,22 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        $this->call(LaratrustSeeder::class);
-       $this->call(FeaturesSeeder::class);
-       $this->call(PlansSeeder::class);
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    //     $this->call(LaratrustSeeder::class);
+    //    $this->call(FeaturesSeeder::class);
+    //    $this->call(PlansSeeder::class);
+       $user = User::factory()->create([
+            'fname' => 'Dev',
+            'email' => 'codedkolobanny@gmail.com',
+            'user_type' => 'superadministrator',
+             'password' => Hash::make('123456789'),
+        ]);
+        if($user)
+        {
+            SuperAdmin::create([
+                     'user_id' => $user->id
+                 ]);
+
+                 $user->addRole($user->user_type);
+        }
     }
 }
